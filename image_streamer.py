f404fa6e7f34ff1f28b2eedb8ffa9b446768f868
Ben Smithers <ben.smithers@bristol.ac.uk>
2015-04-16 17:31:49 +0100

    PDF support and automatic image resize
    Conversion and resize through imagemagick.
    
    Also updated templates.

--- image_streamer.py
+++ image_streamer.py
@@ -3,22 +3,21 @@
 import hashlib
 import json
 import os
-import sys
 import time
 import glob
+import subprocess
+import sys
 
 from flask import Flask, request, redirect,  render_template
 
+class ConvertError (Exception):
+    def __init__(self, message, files):
+        super(ConvertError, self).__init__(message)
+        self.files = files
+
+
 app = Flask(__name__)
 app.config.from_pyfile(os.path.join(app.root_path, 'config.py'))
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
-
-def get_destination_filename(filename):
-    ext = filename.rsplit('.', 1)[1]
-    return hashlib.md5(filename + str(time.time())).hexdigest() + '.' + ext
 
 @app.route('/show')
 def show_view():
@@ -32,11 +31,14 @@ def show_upload_form():
 def upload_file():
     file = request.files['file']
     if file and allowed_file(file.filename):
-        print >> sys.stderr, 'Uploading file'
         filename = get_destination_filename(file.filename)
-        print >> sys.stderr, 'New filename is: ', filename
         file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename))
-        return render_template('success.html', filename=filename)
+
+        try:
+            filename = convert(filename)
+            return render_template('success.html', filename=filename)
+        except ConvertError as e:
+            cleanup(e)
     
     return 'Upload Failed!'
 
@@ -52,8 +54,75 @@ def delete_file():
 @app.route('/listing')
 def get_file_listing():
     files = []
-    for filetype in app.config['ALLOWED_EXTENSIONS']:
+    for filetype in app.config['DISPLAY_EXTENSIONS']:
         files.extend(glob.glob(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'],'*.' + filetype)))
 
     files = map(lambda f: os.path.relpath(f, app.root_path), files)
     return json.dumps(files)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['UPLOAD_EXTENSIONS']
+
+def get_destination_filename(filename):
+    ext = filename.rsplit('.', 1)[1]
+    return hashlib.md5(filename + str(time.time())).hexdigest() + '.' + ext
+
+def convert(filename):
+    """
+    Convert the uploaded file if necessary
+    :param filename:
+    :return: the name of the new file (may be the same)
+    """
+    fullname = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename)
+    base, ext = filename.rsplit('.', 1)
+    if ext not in app.config['DISPLAY_EXTENSIONS']:
+        # Convert to jpeg using imagemagick. Could use the python bindings, but this
+        # will do for now
+        output_file = base + '.jpg'
+        full_output = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], output_file)
+        convert_params = ['convert' , '-density', '300', fullname, full_output]
+        if subprocess.call(convert_params):
+            # Returns 0 on success
+            raise ConvertError("Error during format conversion", [fullname, full_output])
+
+        os.remove(fullname)
+        filename = output_file
+        fullname = full_output
+
+    # Size conversion is non-optional
+    size = "%dx%d" % (app.config['MAX_X'], app.config['MAX_Y'])
+
+    colour = request.form.get('colour', 'black')
+    if colour not in ['black', 'white', 'green']:
+        colour = 'black'
+    if colour == 'green':
+            colour = '#2c882e'
+
+    convert_params = ['mogrify' , '-resize', size + '>', fullname]
+    if subprocess.call(convert_params):
+        raise ConvertError("Error during resize", [fullname])
+
+    convert_params = ['mogrify' , '-gravity', 'center', '-extent', size, '-background', colour, fullname]
+    if subprocess.call(convert_params):
+        raise ConvertError("Error during extent change", [fullname])
+
+    return filename
+
+
+def cleanup(e):
+    """
+    Cleanup after there was an error in the conversion process
+    The Exception contains a list of files in use at the time
+    Cycle through these and remove if they exist
+    :param e:
+    :return:
+    """
+    for f in e.files:
+        try:
+            if os.path.isfile(f):
+                os.remove(f)
+        except OSError:
+            continue
+
+    return
